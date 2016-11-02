828d3ee79907075a42eeb74d20a88d4bb7d7839f
davidstedronsky <davestedronsky@hotmail.com>
2016-11-01 18:15:49 -0600

    moved to starter code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,16 @@
 console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+}
+// Put more code here!
+createBoard();
